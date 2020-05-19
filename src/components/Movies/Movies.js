0e4af698c3f1988cbbe82cfd9bd7d50068559322
Ken Wanjohi <wanjohiken85@gmail.com>
2020-05-19 12:04:27 +0300

    reusable structure component

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Type from './Type/Type';
 import Structure from './Structure/Structure'
 import Styles from './Movies.module.css'
+import Single from './Single/Single';
 import axios from 'axios'
 const BASE_URL = 'https://api.themoviedb.org/3/movie/'
 const apikey = `6d52450de693cb39e47fd26bd1c349da`;
@@ -93,18 +94,104 @@ componentDidMount() {
     }
     render() {
         console.log(this.state)
+        const {
+            comingsoonresults,
+            comingsoonisLoading,
+            comingsoonerror,
+            comingsoonerrorstring,
+            comingsooncurrentpage,
+            comingsoonperpage,
+            nowplayingresults,
+            nowplayingisLoading,
+            nowplayingerror,
+            nowplayingerrorstring,
+            nowplayingcurrentpage,
+            nowplayingperpage,
+            popularresults,
+            popularisLoading,
+            popularerror,
+            popularcurrentpage,
+            popularperpage,
+            popularerrorstring,
+            topratedresults,
+            topratedisLoading,
+            topratederror,
+            topratederrorstring,
+            topratedcurrentpage,
+            topratedperpage,
+    
+        } = this.state
+        function truncateString(str, num=105) {
+            if (str.length <= num) {
+              return str
+            }
+            return str.slice(0, num) + '...'
+          }
+        let pop = null
+        if(popularresults) {
+            pop = popularresults.slice((popularcurrentpage * popularperpage) - popularperpage, popularperpage * popularcurrentpage).map(curr => <Single key={curr.id} title= {curr.title} details={curr.id} link={curr.poster_path} description={truncateString(curr.overview)} />)
+        }
+        let top = null
+        if(topratedresults) {
+            top = topratedresults.slice((topratedcurrentpage * topratedperpage) - topratedperpage, topratedperpage * topratedcurrentpage).map(curr => <Single key={curr.id} title= {curr.title} details={curr.id} link={curr.poster_path} description={truncateString(curr.overview)} />)
+        }
+        let coming = null
+        if(comingsoonresults) {
+            coming = comingsoonresults.slice((comingsooncurrentpage * comingsoonperpage) - comingsoonperpage, comingsoonperpage * comingsooncurrentpage).map(curr => <Single key={curr.id} title= {curr.title} details={curr.id} link={curr.poster_path} description={truncateString(curr.overview)} />)
+        }
+        let now = null
+        if(nowplayingresults) {
+            now = nowplayingresults.slice((nowplayingcurrentpage * nowplayingperpage) - nowplayingperpage, nowplayingperpage * nowplayingcurrentpage).map(curr => <Single key={curr.id} title= {curr.title} details={curr.id} link={curr.poster_path} description={truncateString(curr.overview)} />)
+        }
+   
         return(
             <> 
             <Type>Movies</Type>
            
                 <div className={Styles.card}>
                     <Structure 
-                        changepage ={this.setpageHandler}
-                        popsec="popular"
-                        topsec="toprated"
-                        nowsec="nowplaying"
-                        comingsec="upcoming"
-                        data= {this.state}
+                        section= "popular"
+                        isloading= {popularisLoading}
+                        isError = {popularerror}
+                        errorString = {popularerrorstring}
+                        currentPage = {popularcurrentpage}
+                        results = {popularresults}
+                        changePageHandler = {this.setpageHandler}
+                        data = {pop}
+                        sec = "popular"
+                    />  
+                    <Structure 
+                        section= "nowplaying"
+                        isloading= {nowplayingisLoading}
+                        isError = {nowplayingerror}
+                        errorString = {nowplayingerrorstring}
+                        currentPage = {nowplayingcurrentpage}
+                        results = {nowplayingresults}
+                        changePageHandler = {this.setpageHandler}
+                        data = {now}
+                        sec = "nowplaying"
+                    />  
+                    <Structure 
+                        section= "Upcoming"
+                        isloading= {comingsoonisLoading}
+                        isError = {comingsoonerror}
+                        errorString = {comingsoonerrorstring}
+                        currentPage = {comingsooncurrentpage}
+                        results = {comingsoonresults}
+                        changePageHandler = {this.setpageHandler}
+                        data = {coming}
+                        sec= "coming"
+                    />  
+                    <Structure 
+                        section= "top rated"
+                        isloading= {topratedisLoading}
+                        isError = {topratederror}
+                        errorString = {topratederrorstring}
+                        currentPage = {topratedcurrentpage}
+                        results = {topratedresults}
+                        changePageHandler = {this.setpageHandler}
+                        data = {top}
+                        sec = "toprated"
                     />  
                  </div> 
           
--- src/components/Movies/Structure/Structure.js
+++ src/components/Movies/Structure/Structure.js
@@ -1,150 +1,31 @@
 import React from 'react';
-import Single from '../Single/Single';
 import Styles from './Structure.module.css';
-import Spinner from '../../Spinner/Spinner'
-const structure = (props) => {
-    const {
-        comingsoonresults,
-        comingsoonisLoading,
-        comingsoonerror,
-        comingsoonerrorstring,
-        comingsooncurrentpage,
-        comingsoonperpage,
-        nowplayingresults,
-        nowplayingisLoading,
-        nowplayingerror,
-        nowplayingerrorstring,
-        nowplayingcurrentpage,
-        nowplayingperpage,
-        popularresults,
-        popularisLoading,
-        popularerror,
-        popularcurrentpage,
-        popularperpage,
-        popularerrorstring,
-        topratedresults,
-        topratedisLoading,
-        topratederror,
-        topratederrorstring,
-        topratedcurrentpage,
-        topratedperpage,
+import Spinner from '../../Spinner/Spinner';
+const structure = (props) => {      
+    return(
 
-    } = props.data
-        function truncateString(str, num=105) {
-            if (str.length <= num) {
-              return str
-            }
-            return str.slice(0, num) + '...'
-          }
-          let pop = null
-          if(popularresults) {
-              pop = popularresults.slice((popularcurrentpage * popularperpage) - popularperpage, popularperpage * popularcurrentpage).map(curr => <Single key={curr.id} title= {curr.title} details={curr.id} link={curr.poster_path} description={truncateString(curr.overview)} />)
-          }
-          let top = null
-          if(topratedresults) {
-              top = topratedresults.slice((topratedcurrentpage * topratedperpage) - topratedperpage, topratedperpage * topratedcurrentpage).map(curr => <Single key={curr.id} title= {curr.title} details={curr.id} link={curr.poster_path} description={truncateString(curr.overview)} />)
-          }
-          let coming = null
-          if(comingsoonresults) {
-              coming = comingsoonresults.slice((comingsooncurrentpage * comingsoonperpage) - comingsoonperpage, comingsoonperpage * comingsooncurrentpage).map(curr => <Single key={curr.id} title= {curr.title} details={curr.id} link={curr.poster_path} description={truncateString(curr.overview)} />)
-          }
-          let now = null
-          if(nowplayingresults) {
-              now = nowplayingresults.slice((nowplayingcurrentpage * nowplayingperpage) - nowplayingperpage, nowplayingperpage * nowplayingcurrentpage).map(curr => <Single key={curr.id} title= {curr.title} details={curr.id} link={curr.poster_path} description={truncateString(curr.overview)} />)
-          }
-     
-          
-        return(
-    
-            <>
-            <div className={Styles.card}>
-            <div className={Styles.section}>{props.popsec}</div>
-               {popularisLoading ? 
-                    <Spinner>loading...</Spinner>:
-                    (
-                        (popularerror) ? 
-                        <div>{popularerrorstring}</div> :
-                        <>
-                         <i className={[Styles.left, Styles.arrow, popularcurrentpage === 1? Styles.display : null].join(' ')} onClick={ () => props.changepage('prev', 'popular')}></i>
-                        <i className={[Styles.right, Styles.arrow, popularcurrentpage === (popularresults.length / 4)? Styles.display : null].join(' ')}  onClick={ () => props.changepage('next', 'popular')}></i>
-                        {/* {renderButtons()} */}
-                        <div className={Styles.grid}>
-                         {pop}
-                         </div>
-                         </>
-                    )
-
-                }
+    <div className={Styles.card}>
+    <div className={Styles.section}>{props.section}</div>
+        {props.isloading ? 
+            <Spinner>loading...</Spinner>:
+            (
+                (props.isError) ? 
+                <div>{props.errorstring}</div> :
+                <>
+                <i className={[Styles.left, Styles.arrow, props.currentPage === 1? Styles.display : null].join(' ')} onClick={ () => props.changePageHandler('prev', props.sec)}></i>
+                <i className={[Styles.right, Styles.arrow, props.currentPage === (props.results.length / 4)? Styles.display : null].join(' ')}  onClick={ () => props.changePageHandler('next', props.sec)}></i>
+                {/* {renderButtons()} */}
+                <div className={Styles.grid}>
+                    {props.data}
                 </div>
-             
-            
-            
-            <div className={Styles.card}>
-            <div className={Styles.section}>{props.nowsec}</div>
-              
-                {nowplayingisLoading ? 
-                    <Spinner>loading...</Spinner>:
-                    (
-                        (nowplayingerror) ? 
-                        <div>{nowplayingerrorstring}</div>:
-                        <>
-                         <i className={[Styles.left, Styles.arrow, nowplayingcurrentpage === 1? Styles.display : null].join(' ')}  onClick={ () => props.changepage('prev', 'nowplaying')}></i>
-                        <i className={[Styles.right, Styles.arrow, nowplayingcurrentpage === (nowplayingresults.length / 4)? Styles.display : null].join(' ')}  onClick={ () => props.changepage('next', 'nowplaying')}></i>
-                        <div className={Styles.grid}>
-                        {now}
-                        </div>
-                        </>
-                    )
+                </>
+            )
 
-                }
-                
-            </div>
-
-            <div className={Styles.card}>
-            <div className={Styles.section}>{props.comingsec}</div>
-              
-               {comingsoonisLoading ? 
-                    <Spinner>loading...</Spinner>:
-                    (
-                        (comingsoonerror) ? 
-                        <div>{comingsoonerrorstring}</div>:  
-                        <>
-                         <i className={[Styles.left, Styles.arrow, comingsooncurrentpage === 1? Styles.display : null].join(' ')} onClick={ () => props.changepage('prev', 'coming')} ></i>
-                        <i className={[Styles.right, Styles.arrow, comingsooncurrentpage === (comingsoonresults.length / 4)? Styles.display : null].join(' ')} onClick={ () => props.changepage('next', 'coming')}></i>
-                         <div className={Styles.grid}>
-                        {coming}
-                        </div>
-                        </>
-                    )
-
-                }
-               
-            </div>
-            
-            <div className={Styles.card}>
-            <div className={Styles.section}>{props.topsec}</div>
-              
-                {topratedisLoading ? 
-                    <Spinner>loading...</Spinner>:
-                    (
-                        (topratederror) ? 
-                        <div>{topratederrorstring}</div>: 
-                        <>
-                         <i className={[Styles.left, Styles.arrow, topratedcurrentpage === 1? Styles.display : null].join(' ')}  onClick={ () => props.changepage('prev', 'toprated')}></i>
-                        <i className={[Styles.right, Styles.arrow, topratedcurrentpage === (topratedresults.length / 4)? Styles.display : null].join(' ')}  onClick={ () => props.changepage('next', 'toprated')}></i>
-                        <div className={Styles.grid}>
-                        {top}
-                        </div>
-                        </>
-                    )
-
-                }
-               
-            </div>
-            
-            </>
-        )
-    }
+        }
+        </div>
+       
+    )
+}
 
 
 export default structure;
